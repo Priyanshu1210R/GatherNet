05a5072e2e0228cb59c5c16163462bf6af83ab15
PRIYANSHU RANJAN <148432560+Priyanshu1210R@users.noreply.github.com>
2026-06-14 19:41:27 +0530

    Refactor user model schema definition

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,15 +1,14 @@
-import mongoose from "mongoose";
-import  {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+const userScheme = new Schema(
+    {
+        name: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        token: { type: String }
+    }
+)
 
-const userSchema = new Schema({
-    name: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    token: {type: String},
-})
-
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userScheme);
 
 export { User };
