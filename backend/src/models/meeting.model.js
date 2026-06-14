29dde416007fdcd4112913e78e0ddaddc4ff4ec2
PRIYANSHU RANJAN <148432560+Priyanshu1210R@users.noreply.github.com>
2026-06-14 20:00:20 +0530

    Refactor import statement and format meeting schema

--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,13 +1,11 @@
-import  {Schema} from "mongoose";
-
+import mongoose, { Schema } from "mongoose";
 
 const meetingSchema = new Schema({
-    user_id: {type: String, required: true},
-    meetingCode: {type: String, required: true, unique: true},
-    date: {type: Date, default: Date.now, required: true},
-
-})
+    user_id: { type: String, required: true },
+    meetingCode: { type: String, required: true, unique: true },
+    date: { type: Date, default: Date.now, required: true },
+});
 
 const Meeting = mongoose.model("Meeting", meetingSchema);
 
-export {Meeting}; 
+export { Meeting };
